ec30ca86ce7483697accbd1d3f1c6e0f3fb0a0a1
armanshaikh-2001 <137279013+armanshaikh-2001@users.noreply.github.com>
2023-07-02 18:37:43 +0530

    Add files via upload

---
+++ vowel removal.py
@@ -0,0 +1,16 @@
+string = "programming in python"
+
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+result = ""
+
+for i in range(len(string)):
+    if string[i] not in vowels:
+        result = result + string[i]
+
+print("\nAfter removing Vowels: ", result)
+
+
+
+
+
